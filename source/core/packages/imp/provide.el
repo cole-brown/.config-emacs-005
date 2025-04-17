7169d9e15924bed4cf542a609ffbd654f730d975
Cole Brown <code@brown.dev>
2025-04-17 15:12:24 -0700

    imp/provide - minor refactor

--- source/core/packages/imp/provide.el
+++ source/core/packages/imp/provide.el
@@ -22,7 +22,7 @@
 ;;
 ;; Provide to just Emacs:  `provide'
 ;; Provide to just imp:    `imp-provide'
-;; Provide to imp & Emacs: `imp-provide-with-emacs'
+;; Provide to imp & Emacs: `imp-provide' w/ `imp-only' flag
 ;;
 ;;; Code:
 
@@ -31,28 +31,28 @@
 ;; Public API: Provide
 ;;------------------------------------------------------------------------------
 
-(defun imp-provide-loading? (&optional file-name)
-  "Returns true if loading file.
+;; (defun imp-provide-loading? (&optional file-name)
+;;   "Returns true if loading file.
 
-If FILE-NAME is nil, returns true if loading any file.
-If FILE-NAME is a string, returns true if loading that exact
-(full path to) file name."
-  (if file-name
-      ;; Exactly* that file loading?
-      ;;   * Exactly for this platform (OS).
-      (and load-in-progress
-           (string=
-            (imp--path-platform-agnostic load-file-name)
-            (imp--path-platform-agnostic file-name)))
-    ;; Just anything loading?
-    load-in-progress))
+;; If FILE-NAME is nil, returns true if loading any file.
+;; If FILE-NAME is a string, returns true if loading that exact
+;; (full path to) file name."
+;;   (if file-name
+;;       ;; Exactly* that file loading?
+;;       ;;   * Exactly for this platform (OS).
+;;       (and load-in-progress
+;;            (string=
+;;             (imp--path-platform-agnostic load-file-name)
+;;             (imp--path-platform-agnostic file-name)))
+;;     ;; Just anything loading?
+;;     load-in-progress))
 
 
 (defalias 'imp-provided? 'imp-feature?)
 (defalias 'imp-providedp 'imp-feature?)
 
 
-(defun imp-provide (&rest feature)
+(defun imp-provide-to-imp (&rest feature)
   "Record FEATURE in `imp-features' as having been provided.
 
 Each FEATURE should be one of:
@@ -66,21 +66,23 @@ If you want to provide the feature to emacs as well, you can either:
      - imp will translate the FEATURE symbol chain via `imp-feature-normalize-imp->emacs'.
   2. Do it yourself by also calling Emacs' `provide' with a symbol of your
      choosing."
-  (let ((feature/imp (imp--feature-normalize feature)))
-    (if (null feature/imp)
+  (let ((feature-imp (imp--feature-normalize feature)))
+    (if (null feature-imp)
         (imp--error "imp-provide"
-                        '("No features to provide? "
-                          "input: %S, normalized: %S")
-                        feature
-                        feature/imp)
+                    '("No features to provide? "
+                      "input: %S, normalized: %S")
+                    feature
+                    feature-imp)
 
       (imp--debug "imp-provide" "Providing feature '%S'..."
-                      feature/imp)
-      (imp--feature-add feature/imp))))
-;; (imp-provide :package 'module 'submodule 'feature)
+                  feature-imp)
+      (imp--feature-add feature-imp)
+      ;; Return nomalized feature name instead of `imp-features' tree.
+      feature-imp)))
+;; (imp-provide-to-imp :package 'module 'submodule 'feature)
 
 
-(defun imp-provide-with-emacs (&rest feature)
+(defun imp-provide-to-emacs (&rest feature)
   "Record FEATURE in `imp-features' and in Emacs' `features' (via
 Emacs' `provide') as having been provided.
 
@@ -93,15 +95,36 @@ imp will translate the FEATURE symbol chain via `imp-feature-normalize-imp->emac
 the result for the call to Emacs' `provide'.
 
 Returns the Emacs feature symbol created/used."
-  (imp-provide feature)
-  (let ((feature/emacs (imp-feature-normalize-imp->emacs feature)))
-    (imp--debug "imp-provide-with-emacs" "Providing to emacs as '%S'..."
-                    feature/emacs)
-    (provide feature/emacs)
-    feature/emacs))
+  (let ((feature-emacs (imp-feature-normalize-for-emacs feature)))
+    (imp--debug "imp--provide-to-emacs" "Providing to emacs as '%S'..."
+                feature-emacs)
+    (provide feature-emacs)
+    ;; return normalized feature name
+    feature-emacs))
+
+
+(defun imp-provide (&rest feautre)
+  "Record FEATURE as available to imp and emacs.
+
+imp:   See tree `imp-features'
+Emacs: See list `features' (via `provide')
+
+Each FEATURE should be one of:
+  - A keyword.
+  - A symbol.
+  - A string (will be passed through `imp-feature-normalize').
+
+If you want to provide the feature to emacs as well, you can either:
+  1. Use `imp-provide-with-emacs' instead of this to have it automatically
+     happen.
+     - imp will translate the FEATURE symbol chain via `imp-feature-normalize-imp->emacs'.
+  2. Do it yourself by also calling Emacs' `provide' with a symbol of your
+     choosing."
+  (apply #'imp-provide-to-emacs feature)
+  (apply #'imp-provide-to-imp feature))
 
 
 ;;------------------------------------------------------------------------------
 ;; The End.
 ;;------------------------------------------------------------------------------
-(imp-provide-with-emacs :imp 'provide)
+(imp-provide :imp 'provide)
