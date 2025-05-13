7042fe903098534a5b7dc20b1d2470572744c95a
Cole Brown <code@brown.dev>
2025-05-13 00:44:25 -0700

    imp/test/ - refactor symbol names

--- source/core/packages/imp/test/loading/dont-load.el
+++ source/core/packages/imp/test/loading/dont-load.el
@@ -4,9 +4,10 @@
 
 ;; We expect this to never get evaluated.
 ;; Mirror variable to what 'load.el' has.
-(setq test<imp>:loading:dont-load:loaded t)
+(setq test-imp-load--dont-load.el-loaded t)
 
 
 ;;------------------------------------------------------------------------------
 ;; The End.
 ;;------------------------------------------------------------------------------
+(imp-provide :test 'imp-load (imp-file-current))
--- source/core/packages/imp/test/loading/load.el
+++ source/core/packages/imp/test/loading/load.el
@@ -7,62 +7,63 @@
 ;;------------------------------------------------------------------------------
 
 ;; Mirror variable to what 'dont-load.el' has.
-(setq test<imp>:loading:load:loaded t)
+(setq test-imp-load--load.el-loaded t)
 
 
-(defvar test<imp>:file:loading? nil
-  "Gets set via `test<imp>:load'.")
+(defvar test-imp-file-loading? nil
+  "Gets set via `test-imp-load'.")
 
 
-(defun test<imp>:load:set ()
+(defun test-imp-load-set ()
   "Check whether we're loading a file right now.
 
-Sets `test<imp>:file:loading?' with results from `imp-provide-loading?'."
+Sets `test-imp-file-loading?' with results from `imp-provide-loading?'."
   ;; Use setq so it gets set every time.
-  (setq test<imp>:file:loading?
+  (setq test-imp-file-loading?
         (imp-provide-loading?)))
 
 
-(defun test<imp>:load:unset ()
-  "Remove `test<imp>:file:loading?' from the symbol table.
+(defun test-imp-load-unset ()
+  "Remove `test-imp-file-loading?' from the symbol table.
 
 That is: delete the variable from existance."
-  (makunbound 'test<imp>:file:loading?))
+  (makunbound 'test-imp-file-loading?))
 ;; Probably exists to start off with:
-;;   test<imp>:file:loading?
-;;   (test<imp>:load:unset)
+;;   test-imp-file-loading?
+;;   (test-imp-load-unset)
 ;; And now it should not exist:
-;;   (should-error test<imp>:file:loading?)
+;;   (should-error test-imp-file-loading?)
 
 
 ;;------------------------------
 ;; This exists in `provide.el' instead.
 ;;------------------------------
-;; (defun test<imp>:load:exists? ()
-;;   "Returns t if `test<imp>:file:loading?' exists in the symbol table."
+;; (defun test-imp-load-exists? ()
+;;   "Returns t if `test-imp-file-loading?' exists in the symbol table."
 ;;   (condition-case err
 ;;       (progn
 ;;         ;; Just try to access this. If we can't, `condition-case' will catch the `void-variable' signal.
-;;         test<imp>:file:loading?
+;;         test-imp-file-loading?
 ;;         ;; If we could access it, return `t' because it exists.
 ;;         t)
 ;;     ;; Couldn't access it, so it doesn't exist, so return nil.
 ;;     (void-variable nil)))
 ;; ;; Probably exists to start off with:
-;; ;;   test<imp>:file:loading?
-;; ;;   (test<imp>:load:exists?)
-;; ;;   (test<imp>:load:unset)
+;; ;;   test-imp-file-loading?
+;; ;;   (test-imp-load-exists?)
+;; ;;   (test-imp-load-unset)
 ;; ;; And now it should not exist:
-;; ;;   (test<imp>:load:exists?)
+;; ;;   (test-imp-load-exists?)
 
 
 ;;------------------------------------------------------------------------------
 ;; Set on File Exec.
 ;;------------------------------------------------------------------------------
-(test<imp>:load:set)
+(test-imp-load-set)
 
 
 ;;------------------------------------------------------------------------------
 ;; The End.
 ;;------------------------------------------------------------------------------
-(imp-provide :loading 'load)
+(imp-provide :test 'imp-load (imp-file-current))
+
