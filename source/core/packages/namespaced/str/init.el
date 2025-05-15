e4803b87433e6ceba24c55d58027aa3e09c4254e
Cole Brown <code@brown.dev>
2025-05-15 15:00:41 -0700

    pare `ns/str/init.el` down
    Only load the one file I've worked on so far.

--- source/core/packages/namespaced/str/init.el
+++ source/core/packages/namespaced/str/init.el
@@ -38,7 +38,6 @@
 
 (defgroup str:group nil
   "String manipulation functions."
-  :prefix "str:"
   :group 'tools)
 
 
@@ -53,30 +52,30 @@
 
   (imp:load :feature  '(:str normalize)
             :filename "normalize")
-  (imp:load :feature  '(:str regex)
-            :filename "regex")
-  (imp:load :feature  '(:str buffer)
-            :filename "buffer")
-  (imp:load :feature  '(:str string)
-            :filename "string")
-  (imp:load :feature  '(:str propertize)
-            :filename "propertize")
-  (imp:load :feature  '(:str hash)
-            :filename "hash")
+  ;; (imp:load :feature  '(:str regex)
+  ;;           :filename "regex")
+  ;; (imp:load :feature  '(:str buffer)
+  ;;           :filename "buffer")
+  ;; (imp:load :feature  '(:str string)
+  ;;           :filename "string")
+  ;; (imp:load :feature  '(:str propertize)
+  ;;           :filename "propertize")
+  ;; (imp:load :feature  '(:str hash)
+  ;;           :filename "hash")
 
-  ;; Requires 'normalize', 'regex', and 'string'.
-  (unless (imp:flag? :str -case)
-    (imp:load :feature  '(:str +case)
-              :filename "+case")
+  ;; ;; Requires 'normalize', 'regex', and 'string'.
+  ;; (unless (imp:flag? :str -case)
+  ;;   (imp:load :feature  '(:str +case)
+  ;;             :filename "+case")
 
-    (unless (imp:flag? :str -hydra)
-      (imp:load :feature  '(:str +hydra +case)
-                :filename "+case-hydra")))
+  ;;   (unless (imp:flag? :str -hydra)
+  ;;     (imp:load :feature  '(:str +hydra +case)
+  ;;               :filename "+case-hydra")))
 
-  ;; Requires 'string'.
-  (unless (imp:flag? :str -random)
-    (imp:load :feature  '(:str +random)
-              :filename "+random"))
+  ;; ;; Requires 'string'.
+  ;; (unless (imp:flag? :str -random)
+  ;;   (imp:load :feature  '(:str +random)
+  ;;             :filename "+random"))
 
   ;; End load timing.
   )
@@ -85,4 +84,4 @@
 ;;------------------------------------------------------------------------------
 ;; The End.
 ;;------------------------------------------------------------------------------
-(imp:provide:with-emacs :str)
+(imp-provide :str)
