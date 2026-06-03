431d4485c1754bbbc9416c12358b9bf3270879c2
Cole Brown <code@brown.dev>
2026-06-02 18:30:40 -0700

    created basic config for `markdown-mode`

---
+++ source/user/config/languages/markdown.el
@@ -0,0 +1,49 @@
+;;; user/config/languages/markdown.el --- Mark up with markdown! -*- lexical-binding: t; -*-
+;;
+;; Author:     Cole Brown <http://github/cole-brown>
+;; Maintainer: Cole Brown <code@brown.dev>
+;; URL:        https://github.com/cole-brown/.config-emacs
+;; Created:    2026-06-02
+;; Timestamp:  2026-06-02
+;;
+;; These are not the GNU Emacs droids you're looking for.
+;; We can go about our business.
+;; Move along.
+;;
+;;; Commentary:
+;;
+;;  Markdown Mode
+;;  Markdown Previews
+;;
+;;; Code:
+
+
+;;------------------------------------------------------------------------------
+;; `markdown-mode'
+;;------------------------------------------------------------------------------
+
+;; https://github.com/jrblevin/markdown-mode
+;; https://jblevins.org/projects/markdown-mode/
+(use-package markdown-mode
+  :mode ("README\\.md\\'" . gfm-mode)
+
+  ;;------------------------------
+  :init
+  ;;------------------------------
+  (setq markdown-command "multimarkdown")
+
+  ;; ;;------------------------------
+  ;; :custom
+  ;; ;;------------------------------
+
+  ;;------------------------------
+  :bind
+  ;;------------------------------
+  (:map markdown-mode-map
+   ("C-c C-e" . markdown-do)))
+
+
+;;------------------------------------------------------------------------------
+;; The End.
+;;------------------------------------------------------------------------------
+(imp-provide user (imp-path-relative 'user (imp-path-sans-extension (imp-path-current-file))))
