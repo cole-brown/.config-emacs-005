5b0113880197909091c4d9aac2f0714a8687ead6
Cole Brown <code@brown.dev>
2026-06-16 17:31:15 -0700

    treesit-auto: Add more recipes with `:abi14-revision`

--- source/user/config/languages/treesit.el
+++ source/user/config/languages/treesit.el
@@ -4,7 +4,7 @@
 ;; Maintainer: Cole Brown <code@brown.dev>
 ;; URL:        https://github.com/cole-brown/.config-emacs
 ;; Created:    TODO:(datetime:timestamp:insert :rfc-3339:date)
-;; Timestamp:  2026-06-03
+;; Timestamp:  2026-06-16
 ;;
 ;; These are not the GNU Emacs droids you're looking for.
 ;; We can go about our business.
@@ -83,14 +83,66 @@
   ;; (treesit-library-abi-version)
   ;; Emacs 30.2: treesit ABI v14
 
+  (setq --/treesit-auto/recipe/c
+        (make-treesit-auto-recipe
+         :lang 'c
+         :ts-mode 'c-ts-mode
+         :remap 'c-mode
+         :requires 'cpp
+         :url "https://github.com/tree-sitter/tree-sitter-c"
+         :abi14-revision "v0.21.2"
+         :ext "\\.c\\'"))
+  (add-to-list 'treesit-auto-recipe-list --/treesit-auto/recipe/c)
+
+  (setq --/treesit-auto/recipe/csharp
+        (make-treesit-auto-recipe
+         :lang 'c-sharp
+         :ts-mode 'csharp-ts-mode
+         :remap 'csharp-mode
+         :url "https://github.com/tree-sitter/tree-sitter-c-sharp"
+         :abi14-revision "v0.21.2"
+         :ext "\\.cs\\'"))
+  (add-to-list 'treesit-auto-recipe-list --/treesit-auto/recipe/csharp)
+
+  (setq --/treesit-auto/recipe/html
+        (make-treesit-auto-recipe
+         :lang 'html
+         :ts-mode 'html-ts-mode
+         :remap '(mhtml-mode sgml-mode)
+         :url "https://github.com/tree-sitter/tree-sitter-html"
+         :abi14-revision "v0.21.2"
+         :ext "\\.html\\'"))
+  (add-to-list 'treesit-auto-recipe-list --/treesit-auto/recipe/html)
+
+  (setq --/treesit-auto/recipe/js
+        (make-treesit-auto-recipe
+         :lang 'javascript
+         :ts-mode 'js-ts-mode
+         :remap '(js-mode javascript-mode js2-mode)
+         :url "https://github.com/tree-sitter/tree-sitter-javascript"
+         :abi14-revision "v0.21.2"
+         :ext "\\.js\\'"))
+  (add-to-list 'treesit-auto-recipe-list --/treesit-auto/recipe/js)
+
+  (setq --/treesit-auto/recipe/json
+        (make-treesit-auto-recipe
+         :lang 'json
+         :ts-mode 'json-ts-mode
+         :remap 'js-json-mode
+         :url "https://github.com/tree-sitter/tree-sitter-json"
+         :abi14-revision "v0.21.2"
+         :ext "\\.json\\'"))
+  (add-to-list 'treesit-auto-recipe-list --/treesit-auto/recipe/json)
+
   (setq --/treesit-auto/recipe/rust
         (make-treesit-auto-recipe
          :lang 'rust
          :url "https://github.com/tree-sitter/tree-sitter-rust"
-         :abi14-revision "v0.21.2"))
-
+         :abi14-revision "v0.21.2"
+         :ext "\\.rs\\'"))
   (add-to-list 'treesit-auto-recipe-list --/treesit-auto/recipe/rust)
 
+  ;; Enable globally!
   (global-treesit-auto-mode))
 
 
