a47c817ab4c46de01bd83964ab85cd2c076371b6
Cole Brown <code@brown.dev>
2026-06-25 16:36:59 -0700

    fix todos
    All except the one TODO.

--- source/user/utilities/icons.el
+++ source/user/utilities/icons.el
@@ -1,10 +1,10 @@
-;;; user/config/TODO-FILENAME.el --- TODO-SHORT-DESCRIPTION -*- lexical-binding: t; -*-
+;;; user/utilities/icons.el --- nerd-icon utility funcs -*- lexical-binding: t; -*-
 ;;
 ;; Author:     Cole Brown <http://github/cole-brown>
 ;; Maintainer: Cole Brown <code@brown.dev>
 ;; URL:        https://github.com/cole-brown/.config-emacs
-;; Created:    TODO:(datetime:timestamp:insert :rfc-3339:date)
-;; Timestamp:  2026-06-24
+;; Created:    2026-06-24
+;; Timestamp:  2026-06-25
 ;;
 ;; These are not the GNU Emacs droids you're looking for.
 ;; We can go about our business.
@@ -12,13 +12,10 @@
 ;;
 ;;; Commentary:
 ;;
-;;  TODO-COMMENTARY
+;;  So you can just ask for a Nerd Font icon by name without having to know
+;;  where to ask for it or be interactive about it.
 ;;
 ;;; Code:
-
-;; TODO-INCLUDES
-;; (imp-require unit)
-;; (require 'cl-lib)
 
 (require 'nerd-icons)
 
@@ -26,7 +23,6 @@
 ;;------------------------------------------------------------------------------
 ;; `nerd-icons' private helpers
 ;;------------------------------------------------------------------------------
-
 
 (defun --/icon/properties/icon (plist allow-generic)
   "To be passed directly to `nerd-icons-*icon' funcs."
@@ -91,33 +87,36 @@
 
 For ICON strings see: https://www.nerdfonts.com/cheat-sheet"
   (if-let* ((fields (and (stringp icon)
-                           (string-split icon "-")))
-              (nf-prefix-valid (string= "nf" (nth 0 fields)))
-              (nf-type (nth 1 fields))
-              (nf-name-valid (stringp (nth 2 fields))))
-    (pcase nf-type
-      ("cod" #'nerd-icons-codicon) ; Codicons
-      ("dev" #'nerd-icons-devicon) ; Devicons
-      ((or "fa" "fae") #'nerd-icons-faicon) ; Font Awesome + Font Awesome Extension
-      ("iec" #'nerd-icons-ipsicon) ; IEC Power Symbols
-      ("linux" #'nerd-icons-flicon) ; Font Logos (Formerly Font Linux)
-      ("md" #'nerd-icons-mdicon) ; Material Design Icons
-      ("oct" #'nerd-icons-octicon) ; Octicons
-      ("pom" #'nerd-icons-pomicon) ; Pomicons
-      ("pl" #'nerd-icons-plicon) ; Powerline Extra Symbols
-      ((or "seti" "custom") #'nerd-icons-sucicon) ; Seti-UI + Custom
-      ("weather" #'nerd-icons-wicon) ; Weather Icons
+                         (string-split icon "-")))
+            (nf-prefix-valid (string= "nf" (nth 0 fields)))
+            (nf-type (nth 1 fields))
+            (nf-name-valid (stringp (nth 2 fields))))
+      (pcase nf-type
+        ("cod" #'nerd-icons-codicon) ; Codicons
+        ("dev" #'nerd-icons-devicon) ; Devicons
+        ((or "fa" "fae") #'nerd-icons-faicon) ; Font Awesome + Font Awesome Extension
+        ("iec" #'nerd-icons-ipsicon) ; IEC Power Symbols
+        ("linux" #'nerd-icons-flicon) ; Font Logos (Formerly Font Linux)
+        ("md" #'nerd-icons-mdicon) ; Material Design Icons
+        ("oct" #'nerd-icons-octicon) ; Octicons
+        ("pom" #'nerd-icons-pomicon) ; Pomicons
+        ("pl" #'nerd-icons-plicon) ; Powerline Extra Symbols
+        ((or "seti" "custom") #'nerd-icons-sucicon) ; Seti-UI + Custom
+        ("weather" #'nerd-icons-wicon) ; Weather Icons
 
-      ;; unknown
-      (_ ; TODO: what func for: "indent", "extra", ...?
-       (warn "%S: Unknown nerd-icons type '%s' in ICON string: '%s'"
-             '--/icon/func-for
-             nf-type
-             icon)))
+        ;; TODO: what func for: "indent", "extra", ...?
 
+        ;; unknown type in valid ICON string
+        (_
+         (warn "%S: Unknown nerd-icons type '%s' in ICON string: '%s'"
+               '--/icon/func-for
+               nf-type
+               icon)))
+
+    ;; unknowable ICON string
     (warn "%S: Cannot parse ICON string: '%s'"
-             '--/icon/func-for
-             icon)))
+          '--/icon/func-for
+          icon)))
 ;; (--/icon/func-for "nf-md-source_branch_check")
 ;; (--/icon/func-for "nf-jeff-geoff")
 ;; (--/icon/func-for "jeff")
@@ -128,19 +127,36 @@ For ICON strings see: https://www.nerdfonts.com/cheat-sheet"
 ;;------------------------------------------------------------------------------
 
 (defun /icon/solo (icon &rest plist)
-  "Return nerd-icons function to use for ICON name string.
+  "Return `nerd-icons' function to use for ICON name string.
 
 For ICON strings see: https://www.nerdfonts.com/cheat-sheet
 
-TODO(icons): finish the rest of the docstr
-Optional PLIST:
+Optional PLIST key/value pairs (kvp):
   - `:height'     - number
   - `:v-adjust'   - number (display (raise number))
   - `:icon:face'  - face
   - `:icon:color' - face foreground color
-  - `:face'  - face
-  - `:color' - face foreground color
-"
+  - `:face'       - face
+  - `:color'      - face foreground color
+
+HEIGHT must be `numberp'.
+V-ADJUST must be `numberp'.
+
+The face-related kvps are mutually exclusive, in this priority order:
+  1. ICON:FACE
+  2. ICON:COLOR
+  3. FACE
+  4. COLOR
+
+ICON:FACE & FACE must be valid values for `face' attribute.
+
+ICON:COLOR & COLOR must be valid color values for `face (:foreground COLOR)'
+ex: `red', `#xff0000'
+
+See function `propertize'.
+
+See Info node `(elisp) Face Attributes' for complete information.
+URL `https://www.gnu.org/software/emacs/manual/html_node/elisp/Face-Attributes.html'"
   (when-let ((func (--/icon/func-for icon)))
     (apply func icon (--/icon/properties/icon plist t))))
 ;; (/icon/solo "nf-md-source_branch_check")
@@ -150,22 +166,41 @@ Optional PLIST:
 
 
 (defun /icon/with-text (icon text &rest plist)
-  "Return nerd-icons function to use for ICON name string.
+  "Return `nerd-icons' function to use for ICON name string.
 
 For ICON strings see: https://www.nerdfonts.com/cheat-sheet
 
-TODO(icons): finish the rest of the docstr
-Optional PLIST:
+Optional PLIST key/value pairs (kvp):
+  - `:help-echo'  - help text (eg for mouse hover)
   - `:height'     - number
   - `:v-adjust'   - number (display (raise number))
-  - `:icon:color' - face foreground color
   - `:icon:face'  - face
-  - `:text:color' - face foreground color
+  - `:icon:color' - face foreground color
   - `:text:face'  - face
+  - `:text:color' - face foreground color
   - `:face'       - face
   - `:color'      - face foreground color
-  - `:help:echo'  - string
-"
+
+HELP-ECHO must be `stringp'.
+
+HEIGHT & V-ADJUST are only for the ICON.
+  - HEIGHT must be `numberp'.
+  - V-ADJUST must be `numberp'.
+
+The FACEs override the COLORs.
+  - ICON:* are used to style the ICON character.
+  - TEXT:* are used to style the TEXT string.
+  - FACE/COLOR are used to style everything.
+
+The FACEs must be valid values for `face' attribute.
+
+The COLORs must be valid color values for `face (:foreground COLOR)'
+ex: `red', `#xff0000'
+
+See function `propertize'.
+
+See Info node `(elisp) Face Attributes' for complete information on `face'.
+URL `https://www.gnu.org/software/emacs/manual/html_node/elisp/Face-Attributes.html'"
   (apply #'propertize
          (concat
           ;; icon
